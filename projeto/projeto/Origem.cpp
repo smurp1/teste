04a7c0ba6d04e57e39eb3b99e533bf1fba5008ff
samuel pereira <samuelrpereira123@gmail.com>
2021-03-11 15:38:04 -0300

    calculadora

---
+++ projeto/projeto/Origem.cpp
@@ -0,0 +1,14 @@
+#include <iostream>;
+#include "calculadora.h";
+
+using namespace std;
+
+int main()
+{
+	calculadora* calc = new calculadora;
+	cout << "a soma dos valores e"<<calc->soma( 3.0,5.0 )<<endl;
+	
+
+	return 0;
+}
+
